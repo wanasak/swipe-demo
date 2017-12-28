ef599baa4e9613e1270e09ef31eb0cc406613ee6
Wanasak <u510610433@gmail.com>
2017-12-28 17:06:12 +0700

    customizing individual cards

--- App.js
+++ App.js
@@ -1,13 +1,79 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Card, Button } from "react-native-elements";
 
-import Ball from "./src/components/Ball";
+// import Ball from "./src/components/Ball";
+import Deck from "./src/components/Deck";
+
+// Dummy data
+const DATA = [
+    {
+        id: 1,
+        text: "Card #1",
+        uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg"
+    },
+    {
+        id: 2,
+        text: "Card #2",
+        uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg"
+    },
+    {
+        id: 3,
+        text: "Card #3",
+        uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg"
+    },
+    {
+        id: 4,
+        text: "Card #4",
+        uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg"
+    },
+    {
+        id: 5,
+        text: "Card #5",
+        uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg"
+    },
+    {
+        id: 6,
+        text: "Card #6",
+        uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg"
+    },
+    {
+        id: 7,
+        text: "Card #7",
+        uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg"
+    },
+    {
+        id: 8,
+        text: "Card #8",
+        uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg"
+    }
+];
 
 export default class App extends React.Component {
+    renderCard(item) {
+        return (
+            <Card
+                key={item.id}
+                title={item.text}
+                image={{ uri: item.uri }}
+            >
+                <Text style={{ marginBottom: 10 }}>I can customize the card further.</Text>
+                <Button 
+                    title="View Now!"
+                    icon={{ name: "code" }}
+                    backgroundColor="green"
+                />
+            </Card>
+        );
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Ball />
+                <Deck 
+                    data={DATA}
+                    renderCard={this.renderCard}
+                />
             </View>
         );
     }
@@ -16,6 +82,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
+        backgroundColor: "#fff"
     }
 });
--- src/components/Ball.js
+++ src/components/Ball.js
@@ -3,18 +3,21 @@ import { View, StyleSheet, Animated } from 'react-native'
 
 class Ball extends Component {
     componentWillMount() {
+        // step 1: set the begining position
         this.position = new Animated.ValueXY(0, 0);
+        // step 2: define how does it move
         Animated.spring(this.position, {
             toValue: { x: 200, y: 500 }
         }).start();
     }
 
     render() {
+        // step 3: connect animated to element
         return (
             <Animated.View style={this.position.getLayout()}>
                 <View style={styles.ball} />
             </Animated.View>
-        )
+        );
     };
 };
 
