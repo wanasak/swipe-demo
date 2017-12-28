a7b6f51bc055cedd4e8357bc395699f3f9e5fb62
Wanasak <u510610433@gmail.com>
2017-12-28 18:10:23 +0700

    rotate element

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,9 +1,42 @@
-import React, { Component } from 'react'
-import { View, Animated } from 'react-native'
+import React, { Component } from "react";
+import { View, Animated, PanResponder } from "react-native";
 
 class Deck extends Component {
+    constructor(props) {
+        super(props);
+
+        this.position = new Animated.ValueXY();
+
+        this.panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (evet, gesture) => {
+                this.position.setValue({ x: gesture.dx, y: gesture.dy });
+            },
+            onPanResponderRelease: () => {}
+        });
+    }
+
+    getCardStyle() {
+        return {
+            ...this.position.getLayout(),
+            transform: [{ rotate: '-45deg' }]
+        };
+    }
+
     renderCard() {
-        return this.props.data.map(item => {
+        return this.props.data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style={this.getCardStyle()}
+                        {...this.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                );
+            }
+            
             return this.props.renderCard(item);
         });
     }
@@ -14,7 +47,7 @@ class Deck extends Component {
                 {this.renderCard()}
             </View>
         );
-    };
+    }
 }
 
 export default Deck;
